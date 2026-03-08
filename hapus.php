886569ac881989b4acca2ce253ab2b4916df66e6
putri gisca <112202407172@mhs.dinus.ac.id>
2026-03-08 07:02:36 +0700

    membuat file hapus.php untuk menghapus data barang dari database implementasi dari fitur Delete

---
+++ hapus.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'koneksi.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $stmt = $pdo->prepare("DELETE FROM barang WHERE id = ?");
+    $stmt->execute([$id]);
+}
+
+header("Location: index.php");
+exit;
