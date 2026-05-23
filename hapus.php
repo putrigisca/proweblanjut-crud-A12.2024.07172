419b1923aff3aa4c1d01033e01cf9319bb82c87a
putri gisca <112202407172@mhs.dinus.ac.id>
2026-05-23 21:11:44 +0700

    chore: memulihkan file hapus.php untuk dokumentasi

---
+++ hapus.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'koneksi.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $stmt_foto = $pdo->prepare("SELECT foto FROM barang WHERE id = ?");
+    $stmt_foto->execute([$id]);
+    $barang = $stmt_foto->fetch(PDO::FETCH_ASSOC);
+
+    if ($barang && !empty($barang['foto'])) {
+        $file_path = 'uploads/' . $barang['foto'];
+        if (file_exists($file_path)) {
+            unlink($file_path);
+        }
+    }
+
+    $stmt = $pdo->prepare("DELETE FROM barang WHERE id = ?");
+    $stmt->execute([$id]);
+}
+
+header("Location: index.php");
+exit;
+?>
