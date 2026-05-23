418770fc812a4771e90b65fff097268e0c931cd7
putri gisca <112202407172@mhs.dinus.ac.id>
2026-05-23 17:31:04 +0700

    Refactor: Membuat UserModel untuk memisahkan kueri database pengguna

---
+++ app/models/UserModel.php
@@ -0,0 +1,14 @@
+<?php
+class UserModel {
+    private $pdo; 
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function getUserByUsername($username) {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE username = :username");
+        $stmt->execute([':username' => $username]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
