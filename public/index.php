4bd6388be752c23ffa5e0a7cfddd8ba0815dfadb
putri gisca <112202407172@mhs.dinus.ac.id>
2026-05-23 12:20:55 +0700

    Feat: Menambahkan routing untuk action create dan store pada pintu utama aplikasi

--- public/index.php
+++ public/index.php
@@ -3,6 +3,18 @@ require_once '../config/database.php';
 
 require_once '../app/controllers/BarangController.php';
 
-$controller = new BarangController($pdo); 
-$controller->index();
+$controller = new BarangController(); 
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+switch ($action) {
+    case 'create':
+        $controller->create();
+        break;
+    case 'store':
+        $controller->store();
+        break;
+    default:
+        $controller->index();
+        break;
+}
 ?>
